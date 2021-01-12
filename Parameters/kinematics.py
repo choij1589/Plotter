ad90355e61ed8eba8b67fcffb62145d7119e9e0e
Jin Choi <jin.choi@cern.ch>
2021-01-12 09:53:05 +0100

    Kinematics update

---
+++ Parameters/kinematics.py
@@ -0,0 +1,40 @@
+params = {
+    # Leading prompt muons
+    "1/pt": {
+        "cvs_params": {
+            "grid": True
+        },
+        "hist_params": {
+            "rebin": 5,
+            "x_title": "p_{T}(#mu1)",
+            "x_range": [0., 160.],
+            "y_title": "A.U.",
+            "ratio_title": "x/Default",
+            "ratio_range": [0., 2.0]
+        },
+        "info_params": {
+            "info": "Normalized to 1",
+            "cms_text": "CMS",
+            "extra_text": "Work on progress"
+        }
+    },
+    # Subleading prompt muons
+    "2/pt": {
+        "cvs_params": {
+            "grid": True
+        },
+        "hist_params": {
+            "rebin": 5,
+            "x_title": "p_{T}(#mu2)",
+            "x_range": [0., 160.],
+            "y_title": "A.U.",
+            "ratio_title": "x/Default",
+            "ratio_range": [0., 2.0]
+        },
+        "info_params": {
+            "info": "Normalized to 1",
+            "cms_text": "CMS",
+            "extra_text": "Work on progress"
+        }
+    }
+}
